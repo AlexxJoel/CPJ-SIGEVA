8dfd66ac99ba15ccc5924a8b3f8c12805270894a
PabloSFlores <20213tn013@utez.edu.mx>
2023-12-09 15:25:21 -0600

    [UPDATE] update from incomming changes

--- backend/src/modules/router.ts
+++ backend/src/modules/router.ts
@@ -1,9 +1,9 @@
 import AuthRoutes from "./auth/auth.routes";
-import CategoryRoutes from "./category/category.routes"
 import ProductRoutes from "./product/product.routes"
+import CategoryRoutes from "./category/category.routes"
 
 export default {
     AuthRoutes,
+    ProductRoutes,
     CategoryRoutes,
-    ProductRoutes
 };
