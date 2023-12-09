13156f73726f95e6251b18901f6ea3f86dd06035
AlexxJoel <20213tn019@utez.edu.mx>
2023-12-09 12:14:02 -0600

    [ADD] list general with paginator and filter

---
+++ frontend/src/views/admin/component/AdminLayout.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import AdminNavbar from "@/views/admin/component/AdminNavbar.vue";
+</script>
+
+<template>
+  <div>
+    <AdminNavbar/>
+    <div class="mx-3 mx-md-5 mt-3">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
