d019c6021c1d186191f0f97a0f21aab2fc0529dd
AlexxJoel <20213tn019@utez.edu.mx>
2023-12-10 03:48:16 -0600

    [ADD] login router front

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import {AuthLoginDto, AuthUserDto} from "./auth.dtos";
 import {Errors, ResponseApi, validateError} from "../../config/error_codes";
 import {findAuthUserByUsername} from "./auth.gateway";
 import {generateToken} from "../../config/jwt";
+import {validatePassword} from "../../utils/generalFunctions";
 
 
 const login = async (req: Request, res: Response): Promise<Response> => {
@@ -10,10 +11,9 @@ const login = async (req: Request, res: Response): Promise<Response> => {
         const payload = {...req.body} as AuthLoginDto;
         if (!payload.username || !payload.password) throw Error(Errors.INVALID_FIELDS);
         const user = await findAuthUserByUsername(payload.username);
-        if (!user) throw Error(Errors.NOT_FOUND);
-        /*
-            Descrypt password
-         */
+        if (!user) throw Error(Errors.INCORRECT_CREDENTIALS);
+        if (!await validatePassword(payload.password, user.password ? user.password : '')) throw Error(Errors.INCORRECT_CREDENTIALS);
+        if (!user.status) throw Error(Errors.USER_DISABLED);
 
         const token = generateToken(user);
         const body: ResponseApi<AuthUserDto> = {
@@ -22,15 +22,13 @@ const login = async (req: Request, res: Response): Promise<Response> => {
             message: 'Ok',
             data: {...user, password: undefined}
         }
+
         return res.status(body.code).json({...body, token});
-    }catch (e) {
+    } catch (e) {
         const error = validateError(e as Error)
         return res.status(error.code).json(error)
     }
 }
-
-
-
 
 
 export {
