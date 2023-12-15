f4709acf647191bc5ecf33c57f3e9ab2507ce0ee
AlexxJoel <20213tn019@utez.edu.mx>
2023-12-14 22:32:25 -0600

    [ADD] structure modal

--- frontend/src/views/admin/products/ModalUpdateProduct.vue
+++ frontend/src/views/admin/products/ModalUpdateProduct.vue
@@ -1,135 +1,211 @@
 <template>
-    <div class="modal fade" id="ModalUpdateProduct" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
-        ref="updateProductModal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">
-                        Editar producto
-                    </h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <label for="productName" class="form-label">Nombre</label>
-                        <input type="text" class="form-control" id="productName" placeholder="Anillos de oro"
-                            v-model="product.name" />
-                        <label for="productDescription" class="form-label mt-3">Descripción</label>
-                        <textarea class="form-control" id="productDescription" rows="3"
-                            v-model="product.description"></textarea>
+  <div class="modal fade" id="ModalUpdateCategory" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <form @submit="onSubmit">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Actualizar Producto</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar" @click="resetForm()"></button></div>
+          <div class="modal-body">
 
-                        <div class="row">
-                            <div class="col">
-                                <label for="productQuantity" class="form-label">Cantidad</label>
-                                <input type="number" class="form-control" id="productQuantity" placeholder="10"
-                                    v-model="product.quantity" />
-                            </div>
-                            <div class="col">
-                                <label for="productUnitPrice" class="form-label">Precio por unidad</label>
-                                <input type="number" class="form-control" id="productUnitPrice" placeholder="$15,000"
-                                    v-model="product.unitPrice" />
-                            </div>
-                            <div class="col">
-                                <label for="productCategorie" class="form-label">Categoría</label>
-                                <input type="number" class="form-control" id="productQuantity" placeholder="10"
-                                    v-model="product.categoriesId" />
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button style="visibility: hidden" type="button" class="btn btn-secondary" data-bs-dismiss="modal"
-                        id="closeUpdateProduct" @click="$emit('reloadProduc')">
-                        cerrar
-                    </button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                        Cancelar
-                    </button>
-                    <button type="button" class="btn btn-primary" @click="saveProduct" :disabled="!areAllFieldsFilled()">
-                        Guardar
-                    </button>
-                </div>
+            <div class="row">
+              <div class="col">
+                <label for="productUnitPrice" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="productUnitPrice" placeholder="Nombre(s)"
+                       :class="{ 'is-invalid': nMeta.touched && !nMeta.valid, 'is-valid': nMeta.valid }"
+                       v-model="name" @blur="nBlur"/>
+                <div v-if="!nMeta.valid" class="invalid-feedback">{{ nError }}</div>
+              </div>
             </div>
+
+            <div class="row">
+              <div class="col">
+                <label for="productQuantity" class="form-label">Descripción</label>
+                <input type="text" class="form-control" id="productQuantity" placeholder="Primer apellido"
+                       :class="{ 'is-invalid': sMeta.touched && !sMeta.valid, 'is-valid': sMeta.valid }"
+                       v-model="surname" @blur="sBlur"/>
+                <div v-if="!sMeta.valid" class="invalid-feedback">{{ sError }}</div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col">
+                <label for="productQuantity" class="form-label">Descripción</label>
+                <input type="text" class="form-control" id="productQuantity" placeholder="Primer apellido"
+                       :class="{ 'is-invalid': sMeta.touched && !sMeta.valid, 'is-valid': sMeta.valid }"
+                       v-model="surname" @blur="sBlur"/>
+                <div v-if="!sMeta.valid" class="invalid-feedback">{{ sError }}</div>
+              </div>
+            </div>
+
+
+            <div class="row">
+              <div class="col">
+                <label for="productQuantity" class="form-label">Cantidad</label>
+                <input type="number" class="form-control" id="productQuantity" placeholder="10"
+                />
+              </div>
+              <div class="col">
+                <label for="productUnitPrice" class="form-label">Precio por unidad</label>
+                <input type="number" class="form-control" id="productUnitPrice" placeholder="$15,000"
+                      />
+              </div>
+              <div class="col">
+                <label for="productCategory" class="form-label">Categoría</label>
+                <select class="form-control" id="productCategory" v-model="product.categoriesId">
+                  <option v-for="category in items" :key="category.id" :value="category.id">{{
+                      category.name }}</option>
+                </select>
+              </div>
+            </div>
+
+
+          </div>
+          <div class="modal-footer">
+
+            <button  type="button"  class="btn block me-auto"
+                    :class="(!status?'btn-success':'btn-danger')"
+                    @click="changeStatus(!status, props.itemSelected.id)"
+            >
+              {{!status?'Activar':'Desactivar'}}
+            </button>
+
+            <div>
+              <button type="button" id="closeModal" class="btn btn-danger" data-bs-dismiss="modal"
+                      @click="hideModal">
+                Cerrar
+              </button>
+              <button type="submit" class="btn btn-primary text-secondary" :disabled="isDisabled">
+                Guardar
+              </button>
+            </div>
+          </div>
         </div>
+      </form>
     </div>
+  </div>
 </template>
-  
 
 <script setup lang="ts">
-import { onMounted, ref, inject } from "vue";
+import {inject, getCurrentInstance, computed, onMounted, watch, type PropType, ref} from "vue";
+import * as yup from "yup";
+import {useField, useForm} from "vee-validate";
 import api from "../../../config/http-client.gateway";
+import {type SupplierDto} from "@/modules/supplier/dtos/SupplierDto";
+import {ProductController} from "@/modules/products/product.controller";
+
+
 const Swal = inject("$swal");
+const app = getCurrentInstance();
+const SwalCustom = app?.appContext.config.globalProperties.$swalCustom;
 
-const product = ref({
-    name: "",
-    description: "",
-    quantity: 0,
-    unitPrice: 0,
-    categoriesId: 3
+const props = defineProps({
+  itemSelected: {
+    type: Object as PropType<SupplierDto>,
+    required: true
+  }
+})
+//define emits
+const emits = defineEmits([
+   'reloadSupplier'
+])
+
+const items = ref([])
+
+watch(() => props.itemSelected, (newValue) => {
+  if (newValue) {
+    contact.value = newValue.contact
+    name.value = newValue.person.name
+    surname.value = newValue.person.surname
+    lastname.value = newValue.person.lastname
+  }
+})
+
+// Define validation schema
+const schema = yup.object({
+  contact: yup.number().required('Ingresa el contacto').typeError('Ingresa un número').integer('Ingresa un número').positive('Ingresa un número positivo'),
+  name: yup.string().required('Ingresa el nombre').trim(),
+  surname: yup.string().required('Ingresa el apellido paterno').trim(),
+  lastname: yup.string().nullable(),
 });
-let showModal = ref(true);
 
-const areAllFieldsFilled = () => {
-  return (
-    product.value.name &&
-    product.value.description &&
-    product.value.quantity &&
-    product.value.unitPrice
-  );
-};
+// Use the schema in your component
+let {handleSubmit, resetForm} = useForm({
+  validationSchema: schema,
+  initialValues: {
+    contact: props.itemSelected?.contact,
+    name: props.itemSelected?.person.name,
+    surname: props.itemSelected?.person.surname,
+    lastname: props.itemSelected?.person.lastname,
+  }
+});
+
+let {value: contact, errorMessage: cError, handleBlur: cBlur, meta: cMeta} = useField("contact");
+let {value: name, errorMessage: nError, handleBlur: nBlur, meta: nMeta} = useField("name");
+let {value: surname, errorMessage: sError, handleBlur: sBlur, meta: sMeta} = useField("surname");
+let {value: lastname, errorMessage: lError, handleBlur: lBlur, meta: lMeta} = useField("lastname");
+
+const isDisabled = computed(() => !cMeta.valid || !nMeta.valid || !sMeta.valid || !lMeta.valid)
+
+let onSubmit = handleSubmit(async values => {
+  try {
+    const respQuestion = await SwalCustom.question('¿Segura que desea realizar la acción?', 'Se actualizará el proveedor', 'question', 'Actualizar')
+    if (!respQuestion.isConfirmed) return;
+    SwalCustom.loading('Actualizando proveedor', 'Espere un momento por favor')
+    const res = await api.doPut("/supplier", {
+      id: props.itemSelected?.id,
+      contact: values.contact,
+      person: {
+        id: props.itemSelected?.person.id,
+        name: values.name,
+        surname: values.surname,
+        lastname: values.lastname,
+      },
+    })
+    SwalCustom.close()
+    if (res.data.data) {
+      SwalCustom.successTime('Proveedor actualizado correctamente', '')
+      const closeButton: HTMLElement | null = document.querySelector('#closeModal');
+
+      if (closeButton) {
+        closeButton.click();
+        resetForm()
+        emits('reloadSupplier');
+
+      }
+    }
+
+
+  } catch (error) {
+    SwalCustom.close()
+
+  }
+})
 
 const hideModal = () => {
-    showModal.value = false;
-};
-const saveProduct = async () => {
-    try {
-        Swal.fire({
-            title: "¿Segura que desea realizar la acción?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Aceptar",
-            cancelButtonText: "Cancelar",
-            reverseButtons: true,
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                const res = await api.doPost("/product", {
-                    name: product.value.name,
-                    description: product.value.description,
-                    quantity: product.value.quantity,
-                    unitPrice: product.value.unitPrice,
-                    categoriesId: product.value.categoriesId,
-                });
-                if (res.data.data) {
-                    Swal.fire({
-                        icon: "success",
-                        title: "Acción realizada correctamente",
-                        confirmButtonText: "Aceptar",
-                    });
-                }
-                //se obtiene el boton oculto por DOM
-                const btnCloseModal = document.getElementById("closeUpdateProduct");
-                //se verifica que se encontro el elemento
-                if (btnCloseModal) {
-                    //cerramos el modal con el boton oculto
-                    btnCloseModal.click();
-                    //se limpia la data
-                    product.value.name = "";
-                    product.value.description = "";
-                    product.value.quantity = 0;
-                    product.value.unitPrice = 0;
-                    product.value.categoriesId = 0;
-                }
-            }
-        });
-    } catch (error) {
-        console.log(error);
-    }
+  const closeButton: HTMLElement | null = document.querySelector('[data-bs-dismiss="modal"]');
+  if (closeButton) {
+    closeButton.click();
+  }
+}
+
+
+const getCategories = async () => {
+  try {
+    const response = await ProductController.getAllCategories()
+    console.log(response)
+    items.value = response
+  } catch (error) {
+    console.log("Error al obtener categorias", error);
+  }
 };
 
+
 onMounted(() => {
-    console.log("Se ejecuto el modal");
+  getCategories()
 });
+
 </script>
-<style></style>
+
+
+<style scoped></style>
