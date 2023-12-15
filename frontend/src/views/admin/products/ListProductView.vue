7afd2c9fa2cfad33a7405317a18cb6e9e6aa874e
AlexxJoel <20213tn019@utez.edu.mx>
2023-12-15 00:20:33 -0600

    [ADD] validation product

--- frontend/src/views/admin/products/ListProductView.vue
+++ frontend/src/views/admin/products/ListProductView.vue
@@ -6,14 +6,37 @@ import ModalSaveProduct from "@/views/admin/products/ModalSaveProduct.vue";
 import api from "../../../config/http-client.gateway.ts";
 import NotFoundElements from "@/components/NotFoundElements.vue";
 import SkeletonCards from "@/components/SkeletonCards.vue";
+import type {ProductDto} from "@/modules/products/dto/Product.dto";
 
 const Swal = inject("$swal");
 const isLoading = ref(false);
 
-const selectedProduct = ref(null);
+const selectedProduct = ref({
+  id: 0,
+  name: "",
+  description: "",
+  quantity: 0,
+  price: 0,
+  status: false,
+  category: {
+    id: 0,
+    name: "",
+  },
+})
 
-const editProduct = (product) => {
-  selectedProduct.value = {...product};
+const editProduct = (product : any ) => {
+  selectedProduct.value = {
+    id: product.id,
+    name: product.name,
+    description: product.description,
+    quantity: product.quantity,
+    price: product.uniPrice,
+    status: product.status,
+    category:{
+      id: product.category.id,
+      name: product.category.name
+    } ,
+  }as ProductDto;
 };
 
 let response;
@@ -29,13 +52,6 @@ const getProducts = async () => {
   }
 };
 
-const onSelectedId = ref();
-
-const onSelected = (cardId: number) => {
-  console.log("card", cardId);
-  onSelectedId.value = cardId;
-};
-
 onMounted(getProducts);
 
 const items = ref([]);
@@ -48,14 +64,10 @@ const paginator = ref({
 const search = ref("");
 
 const filterItem = computed(() => {
-  return items.value.filter((item) => {
-    return item.name.toLowerCase().includes(search.value.toLowerCase());
-  });
+  return items.value.filter((item) => item.name.toLowerCase().includes(search.value.toLowerCase()));
 });
 
-const totalPages = computed(() => {
-  return Math.ceil(filterItem.value.length / paginator.value.itemsPerPage);
-});
+const totalPages = computed(() => Math.ceil(filterItem.value.length / paginator.value.itemsPerPage));
 
 const paginatedCards = computed(() => {
   const start =
@@ -67,14 +79,12 @@ const paginatedCards = computed(() => {
 const goToPage = (page: number) => {
   paginator.value.currentPage = page;
   window.scrollTo({top: 0, behavior: "smooth"});
-};
+    };
 
-const reloadProduct = () => {
-  getProducts();
-};
+    const reloadProduct = () => getProducts();
 
-const changeStatus = (productId: number) => {
-  Swal.fire({
+    const changeStatus = (productId: number) => {
+    Swal.fire({
     title: "¿Seguro que desea realizar la acción?",
     icon: "warning",
     showCancelButton: true,
@@ -83,135 +93,135 @@ const changeStatus = (productId: number) => {
     confirmButtonText: "Aceptar",
     cancelButtonText: "Cancelar",
     reverseButtons: true,
-  }).then(async (result) => {
+    }).then(async (result) => {
     if (result.isConfirmed) {
-      const res = await api.doDelete(`/product/${productId}`);
-      if (res.data.data) {
-        Swal.fire({
-          icon: "success",
-          title: "Acción realizada correctamente",
-          confirmButtonText: "Aceptar",
-        });
-      }
+    const res = await api.doDelete(`/product/${productId}`);
+    if (res.data.data) {
+    Swal.fire({
+    icon: "success",
+    title: "Acción realizada correctamente",
+    confirmButtonText: "Aceptar",
+    });
+    }
     }
 
     getProducts();
-  });
-};
-</script>
+    });
+    };
+    </script>
 
-<template>
-  <div>
-    <h1 class="h1 text-primary fw-bold">Productos</h1>
+    <template>
+      <div>
+        <h1 class="h1 text-primary fw-bold">Productos</h1>
 
-    <div class="d-md-flex justify-content-between w-100 mb-3">
-      <div class="">
-        <label class="h4 fw-bold">Buscar producto</label>
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Buscar producto" aria-label="Buscar producto"
-                 v-model="search"/>
-          <span class="input-group-text bg-primary text-secondary" readonly><i class="pi pi-search"></i></span>
+        <div class="d-md-flex justify-content-between w-100 mb-3">
+          <div class="">
+            <label class="h4 fw-bold">Buscar producto</label>
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Buscar producto" aria-label="Buscar producto"
+                     v-model="search"/>
+              <span class="input-group-text bg-primary text-secondary" readonly><i class="pi pi-search"></i></span>
 
+            </div>
+          </div>
+          <div class="d-flex align-items-end justify-content-end mt-2 mt-md-0">
+            <button class="btn btn-primary text-secondary w-100" type="button" data-bs-toggle="modal"
+                    data-bs-target="#ModalSaveProduct">
+              Agregar producto
+            </button>
+          </div>
         </div>
-      </div>
-      <div class="d-flex align-items-end justify-content-end mt-2 mt-md-0">
-        <button class="btn btn-primary text-secondary w-100" type="button" data-bs-toggle="modal"
-                data-bs-target="#ModalSaveProduct">
-          Agregar producto
-        </button>
-      </div>
-    </div>
 
-    <SkeletonCards :loading="isLoading" :quantity-cards="10" :col-lg="4"/>
+        <SkeletonCards :loading="isLoading" :quantity-cards="8" :col-lg="4"/>
 
-    <div v-if="!isLoading && paginatedCards.length == 0">
-      <NotFoundElements/>
-    </div>
-    <div v-else>
-      <main class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2 g-lg-3">
-        <div v-for="card in paginatedCards" :key="card.id">
-          <div class="col">
-            <div class="card itemList position-relative">
-              <div class="dropdown dropstart">
-                <button class="btn position-absolute top-0 end-0 m-0" type="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="pi pi-ellipsis-v"></i>
-                </button>
-                <!-- Menú de opciones -->
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item"
-                        @click="editProduct(card)">
-                      Editar
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" @click="changeStatus(card.id)">
-                      {{ card.status ? "Deshabilitar" : "Habilitar" }}
-                    </a>
-                  </li>
-                  <!-- Agrega más opciones según tus necesidades -->
-                </ul>
-              </div>
+        <div v-if="!isLoading && paginatedCards.length == 0">
+          <NotFoundElements/>
+        </div>
+        <div v-else>
+          <main class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2 g-lg-3">
+            <div v-for="card in paginatedCards" :key="card.id">
+              <div class="col">
+                <div class="card itemList position-relative">
+                  <div class="dropdown dropstart">
+                    <button class="btn position-absolute top-0 end-0 m-0" type="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="pi pi-ellipsis-v"></i>
+                    </button>
+                    <!-- Menú de opciones -->
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a class="dropdown-item"
+                           @click="editProduct(card)">
+                          Editar
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" @click="changeStatus(card.id)">
+                          {{ card.status ? "Deshabilitar" : "Habilitar" }}
+                        </a>
+                      </li>
+                      <!-- Agrega más opciones según tus necesidades -->
+                    </ul>
+                  </div>
 
-              <div class="card-body" style="font-size: 1.1rem; width: 100%;" @click="editProduct(card)" data-bs-target="#ModalUpdateCategory" data-bs-toggle="modal" >
-                <div class="d-flex justify-content-center">
-                  <img src="@/assets/images/cristal.png" class="img-fluid px-4" alt="..." width="210">
-                </div>
-                <div >
-                  <p class="card-text mt-1 m-0 fw-semibold" style="height: auto; font-size: 1.2rem;">
-                    {{ card.name }}
-                  </p>
-                  <span class="card-text mt-2 fw-semibold">Descripción: </span>
-                  <p class="card-text m-0 p-0 text-truncate">
-                    {{ card.description? card.description : 'Sin descripción' }}
-                  </p>
+                  <div class="card-body" style="font-size: 1.1rem; width: 100%;" @click="editProduct(card)" data-bs-target="#ModalUpdateCategory" data-bs-toggle="modal" >
+                    <div class="d-flex justify-content-center">
+                      <img src="@/assets/images/cristal.png" class="img-fluid px-4" alt="..." width="210">
+                    </div>
+                    <div >
+                      <p class="card-text mt-1 m-0 fw-semibold" style="height: auto; font-size: 1.2rem;">
+                        {{ card.name }}
+                      </p>
+                      <span class="card-text mt-2 fw-semibold">Descripción: </span>
+                      <p class="card-text m-0 p-0 text-truncate">
+                        {{ card.description? card.description : 'Sin descripción' }}
+                      </p>
 
-                  <span class="card-text mt-2 fw-semibold">Categoria: </span>
-                  <p class="card-text m-0 p-0 text-truncate">
-                    {{ card.category.name }}
-                  </p>
-                  <div class="mt-2">
-                    <p class="card-text m-0">
-                      <span class="fw-semibold">Cantidad:</span> {{ card.quantity }}
-                    </p>
-                    <p class="card-text m-0">
-                      <span class="fw-semibold">Precio por unidad:</span> {{
-                        card.uniPrice | currencyFormat
-                      }}
-                    </p>
-                    <div class="d-flex justify-content-start mt-2">
-                       <span class="badge" :class="card.status ? 'bg-success' : 'bg-danger'">{{ card.status ? "Activo" : "Inactivo" }}</span>
+                      <span class="card-text mt-2 fw-semibold">Categoria: </span>
+                      <p class="card-text m-0 p-0 text-truncate">
+                        {{ card.category.name }}
+                      </p>
+                      <div class="mt-2">
+                        <p class="card-text m-0">
+                          <span class="fw-semibold">Cantidad:</span> {{ card.quantity }}
+                        </p>
+                        <p class="card-text m-0">
+                          <span class="fw-semibold">Precio por unidad:</span> {{
+                            card.uniPrice | currencyFormat
+                          }}
+                        </p>
+                        <div class="d-flex justify-content-start mt-2">
+                          <span class="badge" :class="card.status ? 'bg-success' : 'bg-danger'">{{ card.status ? "Activo" : "Inactivo" }}</span>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </main>
         </div>
-      </main>
-    </div>
 
-    <footer>
-      <nav aria-label="pagination" class="d-flex justify-content-md-end justify-content-center mt-3">
-        <ul class="pagination">
-          <li class="page-item" :class="{ disabled: paginator.currentPage === 1 }">
-            <a class="page-link" href="#" @click.prevent="goToPage(paginator.currentPage - 1)">Anterior</a>
-          </li>
-          <li class="page-item" v-for="number in totalPages" :key="number"
-              :class="{ active: number === paginator.currentPage }">
-            <a class="page-link" href="#" @click.prevent="goToPage(number)">{{
-                number
-              }}</a>
-          </li>
-          <li class="page-item" :class="{ disabled: paginator.currentPage === totalPages }">
-            <a class="page-link" href="#" @click.prevent="goToPage(paginator.currentPage + 1)">Siguiente</a>
-          </li>
-        </ul>
-      </nav>
-    </footer>
-    <ModalSaveProduct @reloadProduct="reloadProduct" :onSelectedId="onSelectedId"/>
-<!--    <ModalUpdateProduct @reloadProduct="reloadProduct"/>-->
+        <footer>
+          <nav aria-label="pagination" class="d-flex justify-content-md-end justify-content-center mt-3">
+            <ul class="pagination">
+              <li class="page-item" :class="{ disabled: paginator.currentPage === 1 }">
+                <a class="page-link" href="#" @click.prevent="goToPage(paginator.currentPage - 1)">Anterior</a>
+              </li>
+              <li class="page-item" v-for="number in totalPages" :key="number"
+                  :class="{ active: number === paginator.currentPage }">
+                <a class="page-link" href="#" @click.prevent="goToPage(number)">{{
+                    number
+                  }}</a>
+              </li>
+              <li class="page-item" :class="{ disabled: paginator.currentPage === totalPages }">
+                <a class="page-link" href="#" @click.prevent="goToPage(paginator.currentPage + 1)">Siguiente</a>
+              </li>
+            </ul>
+          </nav>
+        </footer>
+        <ModalSaveProduct @reloadProducts="reloadProduct"/>
+        <ModalUpdateProduct v-if="selectedProduct" @reloadProducts="reloadProduct" :item-selected="selectedProduct"/>
   </div>
 </template>
 
--- frontend/src/views/admin/products/ModalUpdateProduct.vue
+++ frontend/src/views/admin/products/ModalUpdateProduct.vue
@@ -5,7 +5,9 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Actualizar Producto</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar" @click="resetForm()"></button></div>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"
+                    @click="resetForm()"></button>
+          </div>
           <div class="modal-body">
 
             <div class="row">
@@ -21,40 +23,34 @@
             <div class="row">
               <div class="col">
                 <label for="productQuantity" class="form-label">Descripción</label>
-                <input type="text" class="form-control" id="productQuantity" placeholder="Primer apellido"
-                       :class="{ 'is-invalid': sMeta.touched && !sMeta.valid, 'is-valid': sMeta.valid }"
-                       v-model="surname" @blur="sBlur"/>
-                <div v-if="!sMeta.valid" class="invalid-feedback">{{ sError }}</div>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="col">
-                <label for="productQuantity" class="form-label">Descripción</label>
-                <input type="text" class="form-control" id="productQuantity" placeholder="Primer apellido"
-                       :class="{ 'is-invalid': sMeta.touched && !sMeta.valid, 'is-valid': sMeta.valid }"
-                       v-model="surname" @blur="sBlur"/>
-                <div v-if="!sMeta.valid" class="invalid-feedback">{{ sError }}</div>
+                <input type="text" class="form-control" id="productQuantity" placeholder="descripción"
+                       :class="{ 'is-invalid': dMeta.touched && !dMeta.valid, 'is-valid': dMeta.valid }"
+                       v-model="description" @blur="dBlur"/>
+                <div v-if="!dMeta.valid" class="invalid-feedback">{{ dError }}</div>
               </div>
             </div>
 
 
             <div class="row">
-              <div class="col">
-                <label for="productQuantity" class="form-label">Cantidad</label>
-                <input type="number" class="form-control" id="productQuantity" placeholder="10"
-                />
-              </div>
               <div class="col">
                 <label for="productUnitPrice" class="form-label">Precio por unidad</label>
-                <input type="number" class="form-control" id="productUnitPrice" placeholder="$15,000"
-                      />
+                <input type="number" class="form-control" id="productUnitPrice" placeholder="$15,000" step="any"
+                       :class="{ 'is-invalid': pMeta.touched && !pMeta.valid, 'is-valid': pMeta.valid }"
+                       v-model="price" @blur="pBlur"/>
+                <div v-if="!pMeta.valid" class="invalid-feedback">{{ pError }}</div>
               </div>
               <div class="col">
                 <label for="productCategory" class="form-label">Categoría</label>
-                <select class="form-control" id="productCategory" v-model="product.categoriesId">
+                <select class="form-control" id="productCategory" v-model="categoriesId"
+                        :class="{ 'is-invalid': cMeta.touched && !cMeta.valid, 'is-valid': cMeta.valid }"
+                        @blur="cBlur">
+                  <option value="0" disabled selected>Seleccione una categoría
+                  </option
+                  >
                   <option v-for="category in items" :key="category.id" :value="category.id">{{
-                      category.name }}</option>
+                      category.name
+                    }}
+                  </option>
                 </select>
               </div>
             </div>
@@ -63,16 +59,15 @@
           </div>
           <div class="modal-footer">
 
-            <button  type="button"  class="btn block me-auto"
+            <button type="button" class="btn block me-auto"
                     :class="(!status?'btn-success':'btn-danger')"
                     @click="changeStatus(!status, props.itemSelected.id)"
             >
-              {{!status?'Activar':'Desactivar'}}
+              {{ !status ? 'Activar' : 'Desactivar' }}
             </button>
 
             <div>
-              <button type="button" id="closeModal" class="btn btn-danger" data-bs-dismiss="modal"
-                      @click="hideModal">
+              <button type="button" id="closeModal" class="btn btn-danger me-2" data-bs-dismiss="modal">
                 Cerrar
               </button>
               <button type="submit" class="btn btn-primary text-secondary" :disabled="isDisabled">
@@ -91,8 +86,9 @@ import {inject, getCurrentInstance, computed, onMounted, watch, type PropType, r
 import * as yup from "yup";
 import {useField, useForm} from "vee-validate";
 import api from "../../../config/http-client.gateway";
-import {type SupplierDto} from "@/modules/supplier/dtos/SupplierDto";
 import {ProductController} from "@/modules/products/product.controller";
+import type {ProductDto} from "@/modules/products/dto/Product.dto";
+import type {UpdateProductDto} from "@/modules/products/dto/UpdateProduct.dto";
 
 
 const Swal = inject("$swal");
@@ -101,80 +97,84 @@ const SwalCustom = app?.appContext.config.globalProperties.$swalCustom;
 
 const props = defineProps({
   itemSelected: {
-    type: Object as PropType<SupplierDto>,
+    type: Object as PropType<ProductDto>,
     required: true
   }
 })
 //define emits
-const emits = defineEmits([
-   'reloadSupplier'
-])
+const emits = defineEmits(['reloadProducts'])
 
 const items = ref([])
 
-watch(() => props.itemSelected, (newValue) => {
+watch(() => props.itemSelected, (newValue: ProductDto) => {
   if (newValue) {
-    contact.value = newValue.contact
-    name.value = newValue.person.name
-    surname.value = newValue.person.surname
-    lastname.value = newValue.person.lastname
+    name.value = newValue.name
+    description.value = newValue.description
+    quantity.value = newValue.quantity
+    price.value = newValue.price
+    categoriesId.value = newValue.category.id
+    status.value = newValue.status
   }
 })
 
 // Define validation schema
 const schema = yup.object({
-  contact: yup.number().required('Ingresa el contacto').typeError('Ingresa un número').integer('Ingresa un número').positive('Ingresa un número positivo'),
-  name: yup.string().required('Ingresa el nombre').trim(),
-  surname: yup.string().required('Ingresa el apellido paterno').trim(),
-  lastname: yup.string().nullable(),
+  name: yup.string().required(),
+  description: yup.string().nullable(),
+  quantity: yup.number().required(),
+  price: yup.number().required().typeError('El precio debe ser un número'),
+  categoriesId: yup.number().required(),
+  status: yup.boolean()
 });
 
 // Use the schema in your component
 let {handleSubmit, resetForm} = useForm({
   validationSchema: schema,
   initialValues: {
-    contact: props.itemSelected?.contact,
-    name: props.itemSelected?.person.name,
-    surname: props.itemSelected?.person.surname,
-    lastname: props.itemSelected?.person.lastname,
+    name: '',
+    description: '',
+    quantity: 0,
+    price: 0,
+    categoriesId: 0,
+    status: false
   }
 });
 
-let {value: contact, errorMessage: cError, handleBlur: cBlur, meta: cMeta} = useField("contact");
 let {value: name, errorMessage: nError, handleBlur: nBlur, meta: nMeta} = useField("name");
-let {value: surname, errorMessage: sError, handleBlur: sBlur, meta: sMeta} = useField("surname");
-let {value: lastname, errorMessage: lError, handleBlur: lBlur, meta: lMeta} = useField("lastname");
+let {value: description, errorMessage: dError, handleBlur: dBlur, meta: dMeta} = useField("description");
+let {value: quantity, errorMessage: qError, handleBlur: qBlur, meta: qMeta} = useField("quantity");
+let {value: price, errorMessage: pError, handleBlur: pBlur, meta: pMeta} = useField("price");
+let {value: categoriesId, errorMessage: cError, handleBlur: cBlur, meta: cMeta} = useField("categoriesId");
+let {value: status, errorMessage: sError, handleBlur: sBlur, meta: sMeta} = useField("status");
 
-const isDisabled = computed(() => !cMeta.valid || !nMeta.valid || !sMeta.valid || !lMeta.valid)
+
+const isDisabled = computed(() => !nMeta.valid || !dMeta.valid || !qMeta.valid || !pMeta.valid || !cMeta.valid)
 
 let onSubmit = handleSubmit(async values => {
   try {
-    const respQuestion = await SwalCustom.question('¿Segura que desea realizar la acción?', 'Se actualizará el proveedor', 'question', 'Actualizar')
+    const respQuestion = await SwalCustom.question('¿Segura que desea realizar la acción?', 'Se actualizará el producto', 'question', 'Actualizar')
     if (!respQuestion.isConfirmed) return;
-    SwalCustom.loading('Actualizando proveedor', 'Espere un momento por favor')
-    const res = await api.doPut("/supplier", {
-      id: props.itemSelected?.id,
-      contact: values.contact,
-      person: {
-        id: props.itemSelected?.person.id,
-        name: values.name,
-        surname: values.surname,
-        lastname: values.lastname,
-      },
-    })
+    SwalCustom.loading('Actualizando producto', 'Espere un momento por favor')
+    const res = await api.doPut("/product", {
+      id: props.itemSelected.id,
+      name: name.value,
+      description: description.value,
+      quantity: quantity.value,
+      unitPrice: price.value,
+      categoriesId: categoriesId.value,
+    } )
     SwalCustom.close()
     if (res.data.data) {
-      SwalCustom.successTime('Proveedor actualizado correctamente', '')
-      const closeButton: HTMLElement | null = document.querySelector('#closeModal');
+      SwalCustom.successTime('Producto actualizado correctamente', '')
 
+      const closeButton: HTMLElement | null = document.querySelector('#closeModal');
       if (closeButton) {
         closeButton.click();
         resetForm()
-        emits('reloadSupplier');
+        emits('reloadProducts');
 
       }
     }
-
 
   } catch (error) {
     SwalCustom.close()
@@ -182,23 +182,44 @@ let onSubmit = handleSubmit(async values => {
   }
 })
 
-const hideModal = () => {
-  const closeButton: HTMLElement | null = document.querySelector('[data-bs-dismiss="modal"]');
-  if (closeButton) {
-    closeButton.click();
-  }
-}
-
-
 const getCategories = async () => {
   try {
     const response = await ProductController.getAllCategories()
-    console.log(response)
     items.value = response
   } catch (error) {
     console.log("Error al obtener categorias", error);
   }
 };
+
+let changeStatus = async (status: boolean, id: number) => {
+  try {
+    const respQuestion = await SwalCustom.question('¿Segura que desea realizar la acción?', `Se ${status ? 'activar' : 'desactivar'}a el producto`, 'warning', `${status ? 'Activar' : 'Desactivar'}`)
+    if (!respQuestion.isConfirmed) return;
+    SwalCustom.loading('Actualizando el producto', 'Espere un momento por favor')
+
+    const res = await api.doDelete(`product/${id}`)
+    SwalCustom.close()
+    if (res.data.data) {
+      SwalCustom.successTime('Producto actualizado correctamente', '')
+      const closeButton: HTMLElement | null = document.querySelector('#closeModal');
+      if (closeButton) {
+        closeButton.click();
+        resetForm()
+        emits('reloadProducts');
+
+      }
+    } else {
+      SwalCustom.close()
+    }
+  } catch (e) {
+    await SwalCustom.error('Error al actualizar el producto', 'Intente de nuevo')
+    console.log(e)
+  } finally {
+    setTimeout(() => {
+      SwalCustom.close()
+    }, 1000)
+  }
+}
 
 
 onMounted(() => {
